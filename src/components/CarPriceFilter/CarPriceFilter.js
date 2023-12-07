20b0602e8bdc67582cf7cc03685c09542639a60d
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-12-07 19:29:18 +0200

    price filter render

--- src/components/CarPriceFilter/CarPriceFilter.js
+++ src/components/CarPriceFilter/CarPriceFilter.js
@@ -1,20 +1,34 @@
 import {
   CarPriceFilterWrapper,
   CarPriceFilterLabel,
+  CarPriceFilterSelect,
 } from './CarPriceFilter.styled';
 
 export const CarPriceFilter = () => {
+  const createOptions = () => {
+    const options = [];
+
+    for (let price = 10; price < 500; price += 10) {
+      options.push(
+        <option key={price} value={price}>
+          {price} $
+        </option>
+      );
+    }
+
+    return options;
+  };
+
   return (
     <CarPriceFilterWrapper>
       {' '}
       <CarPriceFilterLabel htmlFor="pricePerHour">
         Price/ 1 hour
       </CarPriceFilterLabel>
-      <select id="pricePerHour" name="pricePerHour">
-        <option value="basePrice">To $</option>
-        <option value="optionB"> B</option>
-        <option value="optionC"> C</option>
-      </select>
+      <CarPriceFilterSelect id="pricePerHour" name="pricePerHour">
+        <option value="">To $</option>
+        {createOptions()}
+      </CarPriceFilterSelect>
     </CarPriceFilterWrapper>
   );
 };
--- src/components/CarPriceFilter/CarPriceFilter.styled.js
+++ src/components/CarPriceFilter/CarPriceFilter.styled.js
@@ -14,3 +14,20 @@ export const CarPriceFilterLabel = styled.label`
   font-weight: 500;
   line-height: 1.3;
 `;
+
+export const CarPriceFilterSelect = styled.select`
+  display: flex;
+  width: 125px;
+  padding: 14px 18px;
+  align-items: flex-start;
+  gap: 32px;
+  border-radius: 14px;
+  background: #f7f7fb;
+  color: #121417;
+
+  font-family: 'Manrope';
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.1;
+`;
