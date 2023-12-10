f6ab028e9ab109d59f87dd85f6af7268f41bd952
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-12-10 20:23:55 +0200

    добавил лэйаут, саспенс, аутлет и прекидание на главную страницу при неправильном пути

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,19 @@
-import { Header } from 'components/Header/Header';
+import { Navigate } from 'react-router-dom';
 import CatalogPage from 'pages/CatalogPage/CatalogPage';
 import FavoritesPage from 'pages/FavoritesPage/FavoritesPage';
 import HomePage from 'pages/HomePage';
 import { Route, Routes } from 'react-router-dom';
+import { Layout } from 'components/Layout/Layout';
 
 export const App = () => {
   return (
-    <div>
-      <Header />
-      <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 };
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Header } from 'components/Header/Header';
+
+export const Layout = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </main>
+    </>
+  );
+};
